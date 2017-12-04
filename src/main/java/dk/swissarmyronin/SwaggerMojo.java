972cf13769fb7cdd7d4e62b715e2dbd1299a77a9
Mads Hvelplund <mhv@tmnet.dk>
2017-12-04 10:14:02 +0100

    Added javadocs and plugins

--- src/main/java/dk/swissarmyronin/SwaggerMojo.java
+++ src/main/java/dk/swissarmyronin/SwaggerMojo.java
@@ -11,17 +11,31 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
+/**
+ * Packaging Mojo that stores a ".yaml" file in a repository.
+ */
 @Mojo(name = "swagger", defaultPhase = LifecyclePhase.PACKAGE)
 public class SwaggerMojo extends AbstractMojo {
 	@Component
 	private MavenProject project;
 
+	/**
+	 * Optional source dir parameter. This is where the YAML file being deployed is
+	 * expected to be found. It does not have to be inside the service project dir.
+	 */
 	@Parameter(defaultValue = "${project.basedir}", property = "sourceDir", required = true)
 	private File sourceDir;
 
+	/**
+	 * This is the file name of service definition file located in the
+	 * {@link #sourceDir}.
+	 */
 	@Parameter(defaultValue = "${project.artifactId}.yaml", property = "swaggerFile", required = true)
 	private String swaggerFile;
 
+	/**
+	 * Mark the {@link #swaggerFile} as the final artifact produced by this Mojo.
+	 */
 	public void execute() throws MojoExecutionException {
 		try {
 			File file = new File(sourceDir, swaggerFile);
